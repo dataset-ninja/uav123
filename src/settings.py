ac59f0596b0e525c6d14fe661c5671ff709cb9a7
peixed351 <peixed351@gmail.com>
2024-03-02 18:18:30 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -36,7 +36,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://cemse.kaust.edu.sa/ivul/uav123"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 14449501
+PREVIEW_IMAGE_ID: int = 14488324
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/uav123"
